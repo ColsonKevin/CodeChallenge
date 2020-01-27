cb12e7133da4137f3d2f90b90d1ed5485f174e29
vanolucas <lucas.vanoverberghe@akabi.eu>
2020-01-27 04:54:05 +0100

    First structure

---
+++ src/main/java/ImmutableQueue.java
@@ -0,0 +1,20 @@
+public class ImmutableQueue implements Queue {
+
+    private Queue<T> myQueue;
+
+    public ImmutableQueue<T>() {}
+
+    public ImmutableQueue<T> enQueue(T t) {
+    }
+
+    public ImmutableQueue<T> deQueue() {
+    }
+
+    public T head() {}
+
+    public boolean isEmpty() {}
+
+    public Queue<T> enQueue(T t) {
+
+    }
+}
