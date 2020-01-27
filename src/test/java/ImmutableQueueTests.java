32eead179e9b84b418d123458d28ba76289e3e17
kevin colson <kevin.colsons@clearstream.com>
2020-01-27 16:51:41 +0000

    Provides test cases

--- src/test/java/ImmutableQueueTests.java
+++ src/test/java/ImmutableQueueTests.java
@@ -1,5 +1,6 @@
 import junit.framework.TestCase;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 import java.util.NoSuchElementException;
 
 /***
@@ -7,9 +8,32 @@ import java.util.NoSuchElementException;
  */
 public class ImmutableQueueTests extends TestCase {
 
-    @Test(expected = NoSuchElementException.class)
+    /***
+     * Tests if the class is immutable
+     */
     public void testImmutableQueue() {
         ImmutableQueue a = new ImmutableQueue<Integer>();
         ImmutableQueue b = (ImmutableQueue) a.enQueue(2);
+        Assertions.assertThrows(NoSuchElementException.class, () -> {
+            System.out.println(a.head());
+        });
+    }
+
+    /***
+     * Tests functions of the class
+     */
+    public void testQueuesFunctions() {
+        ImmutableQueue<Integer> a = new ImmutableQueue<>();
+        assertTrue(a.isEmpty());
+        ImmutableQueue b = (ImmutableQueue) a.enQueue(0);
+        assertFalse(b.isEmpty());
+        assertEquals((int) b.head(), 0);
+        assertTrue(b.deQueue().isEmpty());
+        ImmutableQueue c = (ImmutableQueue) a.enQueue(5);
+        ImmutableQueue d = (ImmutableQueue) c.enQueue(2);
+        assertFalse(d.isEmpty());
+        assertEquals((int) d.head(), 5);
+        ImmutableQueue e = (ImmutableQueue) d.deQueue();
+        assertEquals((int) e.head(), 2);
     }
 }
